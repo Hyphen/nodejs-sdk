089efbc2882ad1e3a2ebe9edd08689b1adc82845
Jared Wray <me@jaredwray.com>
2025-07-01 19:10:05 -0700

    feat: adding in the Hyphen class with Toggle and NetInfo

--- src/hyphen.ts
+++ src/hyphen.ts
@@ -1,13 +1,28 @@
 import {Hookified, type HookifiedOptions} from 'hookified';
+import {NetInfo} from './net-info.js';
+import {Toggle} from './toggle.js';
 
 export type HyphenOptions = {
 	publicApiKey?: string;
 	apiKey?: string;
+	throwErrors?: boolean;
 } & HookifiedOptions;
 
 export class Hyphen extends Hookified {
-	// eslint-disable-next-line @typescript-eslint/no-useless-constructor
+	private readonly _netInfo: NetInfo;
+	private readonly _toggle: Toggle;
+
 	constructor(options?: HyphenOptions) {
 		super(options);
+		this._netInfo = new NetInfo(options);
+		this._toggle = new Toggle(options);
+	}
+
+	public get netInfo(): NetInfo {
+		return this._netInfo;
+	}
+
+	public get toggle(): Toggle {
+		return this._toggle;
 	}
 }
--- test/hyphen.test.ts
+++ test/hyphen.test.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import {describe, test, expect} from 'vitest';
 import {Hyphen} from '../src/hyphen.js';
+import {NetInfo} from '../src/net-info.js';
+import {Toggle} from '../src/toggle.js';
 
 describe('Hyphen', () => {
 	test('should create an instance of Hyphen', () => {
@@ -12,5 +14,17 @@ describe('Hyphen', () => {
 		const options = {publicApiKey: 'test-key'};
 		const hyphen = new Hyphen(options);
 		expect(hyphen).toBeInstanceOf(Hyphen);
+	});
+
+	test('should have a netInfo property', () => {
+		const hyphen = new Hyphen();
+		expect(hyphen.netInfo).toBeDefined();
+		expect(hyphen.netInfo).toBeInstanceOf(NetInfo);
+	});
+
+	test('should have a toggle property', () => {
+		const hyphen = new Hyphen();
+		expect(hyphen.toggle).toBeDefined();
+		expect(hyphen.toggle).toBeInstanceOf(Toggle);
 	});
 });
