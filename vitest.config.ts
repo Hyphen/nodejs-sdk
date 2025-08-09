11017992304dbaa29e6fc3865e7738ab9460b28f
Jared Wray <me@jaredwray.com>
2025-08-09 11:51:31 -0700

    feat: adding in codecoverage badge

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
 	test: {
 		coverage: {
+			reporter: ['text', 'json', 'lcov'],
 			exclude: [
 				'dist/**',
 				'vitest.config.ts',
